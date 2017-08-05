49609ef606fa40f8e3a58596c7f98b7bb8081eca
sraja813 <it8131995@gmail.com>
2017-08-05 12:19:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/home',function(req, res){
-res.sendFile(path.join(__dirname, '/', 'home.html'));  
+res.sendFile(path.join(__dirname, 'ui', 'home.html'));  
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
